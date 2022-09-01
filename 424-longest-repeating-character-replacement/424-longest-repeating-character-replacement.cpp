0eee56d336d16b4de8e61aecae565fc4f62048d0
Harshit Sharma <47037260+harshit9270@users.noreply.github.com>
2022-09-01 07:11:01 +0530

    Time: 10 ms (80.44%), Space: 6.8 MB (97.36%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -7,41 +7,37 @@ public:
         vector<int> frequency(26,0);    
         
         
-        int end = 0;     // End pointer of our window
-        int start = 0;   // Start pointer of our window
+        int end = 0;     // Start pointer of our window
+        int start = 0;   // End pointer of our window
         
         
-        /* Store max frequency within window */
-        int max_freq_within_window = 0; 
+        /* Store max frequency overall */
+        int max_freq = 0; 
         
-        int ans = 0;  // our final answer to return
+        int ans = 0;
         
         while(end < s.length())
         {
             frequency[s[end] - 'A']++;  // updating frequency of character
             
-            max_freq_within_window = max(max_freq_within_window, frequency[s[end] - 'A']);
+            max_freq = max(max_freq, frequency[s[end] - 'A']);
             
             /* if our window size - max_freq > k 
             *    meaning -> we change more character then needed, we will shrink our window.
-            *    See example below.
             *     
             *    end - start + 1 = current window size.
             */
             
-            while(end - start + 1 - max_freq_within_window > k)
+			/* The while loop is removed as this will run only once */
+            if(end - start + 1 - max_freq > k)
             {
                 ans = max(ans, end-start);
                 frequency[s[start] - 'A']--;
                 start++; // reducing window.
-                
-                /* since we removed one element from window, we should update our 
-                   max_freq_within_window variable too.
-                */
-                max_freq_within_window = *max_element(frequency.begin(),frequency.end());
             }
             end++;
         }
+        
         ans = max(ans, end-start);
         return ans;
         
