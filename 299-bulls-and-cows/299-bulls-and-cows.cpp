099ee3bb97cb836c29ceea7336c1ec163342e481
Harshit Sharma <47037260+harshit9270@users.noreply.github.com>
2022-09-01 07:30:54 +0530

    Time: 6 ms (62.09%), Space: 6.7 MB (21.12%) - LeetHub

---
+++ 299-bulls-and-cows/299-bulls-and-cows.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string getHint(string secret, string guess) {
+        map<char,int> mp;
+        int n=secret.size();
+        int b=0,c=0;
+        for(int i=0;i<n;i++)
+        {
+            if(secret[i]==guess[i])
+                b++;
+            else
+                mp[secret[i]]++;
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(secret[i]!=guess[i] && mp.find(guess[i])!=mp.end() && mp[guess[i]]!=0)
+            {
+                c++;
+                mp[guess[i]]--;
+            }
+        }
+        
+        string ans=to_string(b)+"A"+to_string(c)+"B";
+        return ans;
+        
+    }
+};
