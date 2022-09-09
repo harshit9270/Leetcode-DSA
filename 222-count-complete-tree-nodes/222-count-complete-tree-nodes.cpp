1a24cc6dd6c119b3eb07700ac264336cd4d1df44
Harshit Sharma <47037260+harshit9270@users.noreply.github.com>
2022-09-09 21:00:46 +0530

    Time: 63 ms (29.50%), Space: 31.6 MB (6.68%) - LeetHub

--- 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
@@ -11,10 +11,30 @@
  */
 class Solution {
 public:
+    // Using Level order traversal
     int countNodes(TreeNode* root) {
-        if(!root)
-            return 0;
-        
-        return countNodes(root->left) + countNodes(root->right) + 1;
+        if(root == NULL)
+        return 0;
+    
+        int res = 0;
+        queue<TreeNode *> q;
+        q.push(root);
+        while(!q.empty())
+        {
+
+            for(int i = 0; i < q.size(); ++ i)
+            {
+                 ++ res;
+                TreeNode *p = q.front();
+                q.pop();
+
+                if(p -> left != NULL)
+                    q.push(p -> left);
+                if(p -> right != NULL)
+                    q.push(p -> right);
+            }
+        }
+
+        return res;
     }
 };
