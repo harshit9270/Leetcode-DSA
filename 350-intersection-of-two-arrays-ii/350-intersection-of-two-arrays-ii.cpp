c80eb56e45723c76733d35c8a6572312d86a838d
Harshit Sharma <47037260+harshit9270@users.noreply.github.com>
2022-09-10 08:13:17 +0530

    Time: 16 ms (19.26%), Space: 23.6 MB (5.38%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
@@ -1,21 +1,51 @@
 class Solution {
+    // Using binary search
+private:
+   int binSearch(vector<int> n, int lo, int hi, int target)
+   {
+       while(lo<=hi)
+       {
+           int mid = lo + (hi-lo)/2;
+           if(n[mid]== target )
+           {
+               return mid;
+
+           }
+           else if(n[mid] > target)
+           {
+               //left
+               hi = mid -1;
+           }
+           else
+           {
+               lo = mid +1;
+           }
+       }
+       return -1;
+   }
+       
 public:
-    // Using map
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int, int> mp;
-        vector<int> output;
+        vector<int> res;
+        if(nums2.size()< nums1.size())
+            return intersect(nums2, nums1);
+
+        sort(nums1.begin(), nums1.end()) ;
+        sort(nums2.begin(), nums2.end());
         
-        // inserting all elements along with freq of nums1 into map
-        for(auto i : nums1)
-            mp[i] ++;
-        
-        for(int i=0; i<nums2.size(); i++){
-            if(mp.find(nums2[i]) != mp.end() && mp[nums2[i]] > 0){
-                output.push_back(nums2[i]);
-                mp[nums2[i]] --;
-            }                
+        for(int i =0; i < nums1.size();i++)
+        {
+            int curr = nums1[i];
+            int idx = binSearch(nums2, 0, nums2.size()-1, curr);
+            if(idx !=-1)
+            {
+                // found in bigger array
+                res.push_back(curr);
+                // remove from nums2
+                nums2.erase(nums2.begin()+idx);
+
+            }
         }
-        
-        return output;        
+        return res;
     }
 };
