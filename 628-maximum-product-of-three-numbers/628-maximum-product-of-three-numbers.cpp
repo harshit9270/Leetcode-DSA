4e17b79ef42171ecffeea04c038f2dfb03d225bc
Harshit Sharma <47037260+harshit9270@users.noreply.github.com>
2022-09-16 11:11:23 +0530

    Time: 36 ms (99.24%), Space: 27.7 MB (79.82%) - LeetHub

--- 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.cpp
+++ 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.cpp
@@ -1,15 +1,35 @@
-// Sorting Approach
+// Single Scan Approach
 class Solution {
 public:
     int maximumProduct(vector<int>& nums) {
-        int n = nums.size();
-        int maxProduct = INT_MIN;
+        int min1 = INT_MAX,
+        min2 = INT_MAX,
+        max1 = INT_MIN,
+        max2 = INT_MIN,
+        max3 = INT_MIN;
         
-        sort(nums.begin(), nums.end());
+        for(int i=0; i<nums.size(); i++) {
+            if (nums[i] <= min1) {
+                min2 = min1;
+                min1 = nums[i];
+            } 
+            else if (nums[i] <= min2)      // n lies between min1 and min2
+                min2 = nums[i];
+            
+            if (nums[i] >= max1) {            // n is greater than max1, max2 and max3
+                max3 = max2;
+                max2 = max1;
+                max1 = nums[i];
+            } 
+            else if (nums[i] >= max2) {     // n lies betweeen max1 and max2
+                max3 = max2;
+                max2 = nums[i];
+            } 
+            else if (nums[i] >= max3)      // n lies betwen max2 and max3
+                max3 = nums[i];
+            
+        }
         
-        maxProduct = max(maxProduct, nums[0]*nums[1]*nums[n-1]);
-        maxProduct = max(maxProduct, nums[n-3]*nums[n-2]*nums[n-1]);
-                                                              
-        return maxProduct;
+        return max(min1 * min2 * max1, max1 * max2 * max3);
     }
 };
