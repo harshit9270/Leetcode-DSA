fea13bb8aa4fa30958585974ed9917b34bc2cd5a
Harshit Sharma <47037260+harshit9270@users.noreply.github.com>
2022-09-18 21:51:57 +0530

    Time: 71 ms (9.19%), Space: 22.3 MB (91.37%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -1,15 +1,34 @@
-// Using map
+// Using Binary Search
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        unordered_map<int, int> mp;
+        int start = 0, end = nums.size()-1;
         
-        for(int i : nums)
-            mp[i] ++;
+        if(nums.size() == 1)    return nums[0]; 
+        if(nums[0] != nums[1]) return nums[0];
+        if(nums[end] != nums[end-1]) return nums[end];
         
-        for(auto it : mp)
-            if(it.second == 1)
-                return it.first;
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            
+            if(nums[mid] != nums[mid+1] && nums[mid] != nums[mid-1])
+                return nums[mid];
+            
+            if(mid%2 == 0)
+            {
+                if(nums[mid] == nums[mid+1])
+                    start = mid+1;
+                else
+                    end = mid-1;
+            }
+            else 
+            {
+                if(nums[mid] == nums[mid+1])
+                    end = mid-1;
+                else
+                    start = mid+1;
+            }
+        }
         
         return 0;
     }
