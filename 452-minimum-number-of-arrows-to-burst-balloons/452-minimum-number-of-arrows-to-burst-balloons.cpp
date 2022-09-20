e00e76f094a2f98529704e7f40384b303d427d66
Harshit Sharma <47037260+harshit9270@users.noreply.github.com>
2022-09-20 20:56:41 +0530

    Time: 1086 ms (7.49%), Space: 89.8 MB (27.76%) - LeetHub

--- 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -1,3 +1,14 @@
+// All the balloons need to bursted, so in worst case
+// we can use N arrows and burst all. To optimize arrows
+// we need to shoot at points with max number of intersecting
+// balloons, but finding that makes the problem look very difficult.
+// If we observe carefully and look at this sentence "All the balloons need to be burst",
+// we can find a clue, since each balloon needs to burst anyway, we can shoot at its end point 
+// and allow any balloons which also gets covered also be bursted. If we chose a point before the 
+// end point then we might have missed a balloon intersection towards the end. 
+// One important thing is to start wrt to the end pt and not start pt, that way we will
+// know the extent till which a particular balloon can cover.
+
 class Solution {
     static bool cmp(vector<int> &a, vector<int> &b){
         return a[1] < b[1];
