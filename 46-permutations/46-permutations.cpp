da2abbce392578934618aa4ee4efd46a0a02d397
Harshit Sharma <47037260+harshit9270@users.noreply.github.com>
2022-09-26 14:54:11 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (78.37%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,19 +1,35 @@
-// Using STL function next_permutation
-
 class Solution {
 public:
-    
-    vector<vector<int>> permute(vector<int>& nums) 
-    {
-        vector<vector<int>> ourResult;
-        vector<int> singleIter = nums;
-		ourResult.push_back(singleIter);
-        next_permutation(singleIter.begin(), singleIter.end());
-        while (singleIter != nums)
-        {
-            ourResult.push_back(singleIter);
-            next_permutation(singleIter.begin(), singleIter.end());
+    void nextPermutation(vector<int>& nums) {
+	    // Optimal Solution Time O(N) & Auxiliary Space O(1)
+	    // Narayana Panditha's Algorithm
+        int len=nums.size(), k=0, l=0;
+        for(k=len-2;k>=0;k--){
+            if(nums[k]<nums[k+1])
+                break;
         }
-        return ourResult;
+        if(k<0){
+            reverse(nums.begin(),nums.end());
+        }
+        else{
+            for(l=len-1;l>k;l--){
+                if(nums[l]>nums[k])
+                    break;
+        }
+            swap(nums[k],nums[l]);
+            reverse(nums.begin()+k+1,nums.end());
+        }
+    }
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<int> temp = nums;
+        vector<vector<int>> ans;
+        
+        do{
+            nextPermutation(nums);
+            ans.push_back(nums);
+        }while(temp != nums);
+        
+        return ans;
     }
 };
