2b9f82c41d2162b525fff80295cbc99c2d246448
Harshit Sharma <47037260+harshit9270@users.noreply.github.com>
2022-10-05 22:01:25 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (96.78%) - LeetHub

---
+++ 1247-minimum-swaps-to-make-strings-equal/1247-minimum-swaps-to-make-strings-equal.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minimumSwap(string s1, string s2) {
+        int cnt1=0,cnt2=0,i=0;
+        
+        while(i < s1.size()){
+            int x = s1[i];
+            int y = s2[i++];
+            
+            if(x=='x' and y=='y') 
+                cnt1++;
+            if(x=='y' and y=='x') 
+                cnt2++;
+        }
+        
+        if((cnt1+cnt2)%2==1) 
+            return -1;
+        
+        return (cnt1+1)/2 + (cnt2+1)/2;
+    }
+};
