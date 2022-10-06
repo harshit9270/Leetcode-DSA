7b0fec6802763fc5044075db705d1ec83c0c704a
Harshit Sharma <47037260+harshit9270@users.noreply.github.com>
2022-10-06 08:31:57 +0530

    Time: 37 ms (5.28%), Space: 7.5 MB (13.31%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,23 @@
+// Using hashmap
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() > t.length())
+            return isAnagram(t, s);
+        
+        unordered_map<char, int> mp;
+        
+        for(char ch : s)
+            mp[ch] ++;
+        
+        for(char ch : t){
+            if(mp.find(ch) != mp.end() && mp[ch] > 0){
+                mp[ch] --;
+            }
+            else
+                return false;
+        }
+        
+        return true;
+    }
+};
