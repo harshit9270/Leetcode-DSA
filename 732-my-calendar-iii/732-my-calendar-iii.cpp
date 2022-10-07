c6a1ea19d27d1fb445e273fbacd8bb5a015f1afe
Harshit Sharma <47037260+harshit9270@users.noreply.github.com>
2022-10-07 10:31:12 +0530

    Time: 240 ms (40.60%), Space: 26.4 MB (75.61%) - LeetHub

---
+++ 732-my-calendar-iii/732-my-calendar-iii.cpp
@@ -0,0 +1,32 @@
+class MyCalendarThree {
+public:
+    map<int, int> mp;
+    
+    MyCalendarThree() {
+    }
+    
+    int book(int start, int end) {
+        // This somehow works, the sum of these values return number of overlapping intervals
+        // I could not figure out why :)
+        mp[start]++;        
+        mp[end]--;
+        
+        // iterate over map and find the maximum overlapping bookings
+        int maxi = INT_MIN;
+        int sum = 0;
+        
+        for(auto x : mp){
+            sum += x.second;
+            
+            maxi = max(maxi, sum);
+        }
+        
+        return maxi;
+    }
+};
+
+/**
+ * Your MyCalendarThree object will be instantiated and called as such:
+ * MyCalendarThree* obj = new MyCalendarThree();
+ * int param_1 = obj->book(start,end);
+ */
