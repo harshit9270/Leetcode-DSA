e62fb6de5c0a6b41e71a05931c0f949789699d50
Harshit Sharma <47037260+harshit9270@users.noreply.github.com>
2022-10-07 11:01:53 +0530

    Time: 351 ms (73.66%), Space: 72.6 MB (39.51%) - LeetHub

---
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -0,0 +1,32 @@
+// Using priority queue
+typedef pair<double, vector<int>> piv;
+
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<piv, vector<piv>> pq;
+        
+        for(int i=0; i<k; i++){
+            double distance = pow(double(pow(points[i][0], 2) + pow(points[i][1], 2)), 0.5);
+            
+            pq.push(make_pair(distance, points[i]));
+        }
+        
+        for(int i=k; i<points.size(); i++){
+            double distance = pow(double(pow(points[i][0], 2) + pow(points[i][1], 2)), 0.5);
+            
+            if(pq.top().first > distance){
+                pq.pop();
+                pq.push(make_pair(distance, points[i]));
+            }
+        }
+        
+        vector<vector<int>> res;
+        while (!pq.empty()) {
+            res.push_back(pq.top().second);
+            pq.pop();
+        }
+        
+        return res;
+    }
+};
