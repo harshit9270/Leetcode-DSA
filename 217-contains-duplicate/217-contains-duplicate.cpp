b579c37398137eae4f49cd2aa0bd351dd3131818
Harshit Sharma <47037260+harshit9270@users.noreply.github.com>
2022-10-07 08:25:33 +0530

    Time: 86 ms (98.94%), Space: 51.3 MB (64.86%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -0,0 +1,16 @@
+// Using hashmap
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> freq;
+        
+        for(int i : nums){
+            freq[i] ++;
+            
+            if(freq[i] > 1)
+                return true;
+        }
+        
+        return false;
+    }
+};
