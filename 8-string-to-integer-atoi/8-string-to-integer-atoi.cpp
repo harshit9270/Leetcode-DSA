1fd3b2846ffa8fae2133810b3656ba2f877a7a39
Harshit Sharma <47037260+harshit9270@users.noreply.github.com>
2022-10-07 18:40:08 +0530

    Time: 15 ms (5.48%), Space: 7.1 MB (12.68%) - LeetHub

---
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
@@ -0,0 +1,12 @@
+// Using stringstream ✌
+class Solution {
+public:
+    int myAtoi(string str) {
+        
+        stringstream obj(str); 
+        int ans=0;
+        obj>>ans; 
+       
+        return ans; 
+    }
+};
