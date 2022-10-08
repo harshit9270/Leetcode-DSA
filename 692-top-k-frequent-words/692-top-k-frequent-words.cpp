c1439b1fef27fd1cc6c1ac00062e9733c8cca566
Harshit Sharma <47037260+harshit9270@users.noreply.github.com>
2022-10-08 10:18:23 +0530

    Time: 21 ms (60.30%), Space: 12.6 MB (76.08%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.cpp
+++ 692-top-k-frequent-words/692-top-k-frequent-words.cpp
@@ -1,32 +1,29 @@
+// Using min heap
+// Adding '-' sign to the value entered in priority queue makes it a min heap
+
 class Solution {
-public:
-    // Using map and vector<pair>
-    static bool comparator(pair<string,int> p1, pair<string,int> p2)
-    {
-        if(p1.second>p2.second || (p1.second==p2.second && p1.first<p2.first))
-            return true;
-        return false;
-    }
-    
-    vector<string> topKFrequent(vector<string>& words, int k) {
-        unordered_map<string,int> m1;
-        
-        for(int i=0; i<words.size(); i++)
-            m1[words[i]]++;
-        
-        vector<pair<string,int>> v1;
-        for(auto it=m1.begin(); it!=m1.end(); it++)
-            v1.push_back({it->first,it->second});
-        
-        sort(v1.begin(),v1.end(),comparator);
-        
-        vector<string> ans;
-        for(int i=0; i<k; i++)
-        {
-            ans.push_back(v1[i].first);
-        }
-        
-        return ans;
-    }
-    
-};
+	public:
+		vector<string> topKFrequent(vector<string>& words, int k) {
+			unordered_map <string, int> umap;
+			for (string s: words) 
+                umap[s]++;
+			
+            priority_queue <pair<int, string>> pq;
+			
+            for (auto it: umap) {
+				// minus sign used to build min heap 
+				pq.push({-it.second, it.first});
+                
+				if(pq.size() > k) 
+                    pq.pop();
+			}
+			
+            vector <string> res;
+			while (k--) {
+				res.insert(res.begin(), pq.top().second);
+				pq.pop();
+			}
+			
+            return res;
+		}
+	};
