09a50afc898fd7deda23ca4637d13bb547d334c2
Harshit Sharma <47037260+harshit9270@users.noreply.github.com>
2022-11-07 22:54:58 +0530

    Time: 211 ms (36.71%), Space: 14.4 MB (78.44%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,30 +1,20 @@
-// Top-down DP Approach
+// Bottom-up DP Approach
 class Solution {
-    int solveRec(vector<int> & coins, int amount, vector<int> &dp){
-        if(amount == 0)
-            return 0;
-        if(amount < 0)
-            return INT_MAX;
+    int solveTab(vector<int> & coins, int amount){
+        vector<int> dp(amount+1, INT_MAX);
         
-        if(dp[amount] != -1)
-            return dp[amount];
+        dp[0] = 0;
         
-        int mini = INT_MAX;
-        for(int i=0; i<coins.size(); i++){
-            int ans = solveRec(coins, amount-coins[i], dp);
-            if(ans != INT_MAX)
-                mini = min(mini, 1+ans);
+        for(int i=1; i<=amount; i++){
+            for(int j=0; j<coins.size(); j++)
+               if(i-coins[j] >= 0 && dp[i-coins[j]] != INT_MAX)
+                    dp[i] = min(dp[i], 1+dp[i-coins[j]]);
         }
         
-        dp[amount] = mini;
-        
-        return dp[amount];
+        return (dp[amount] == INT_MAX) ? -1 : dp[amount];
     }
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount+1, -1);
-        
-        int ans = solveRec(coins, amount, dp);
-        return (ans == INT_MAX) ? -1 : ans;
+        return solveTab(coins, amount);
     }
 };
