15c372ccb1a3cfbfb8c8ffbc33baccc9685fc17f
Harshit Sharma <47037260+harshit9270@users.noreply.github.com>
2022-11-13 21:42:12 +0530

    Time: 409 ms (46.38%), Space: 9 MB (70.09%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,25 +1,22 @@
-// Top-down DP Approach
+// Bottom-up DP Approach
 class Solution {
-    int solveMem(int n, vector<int> & dp){
-        if(n == 0)
-            return 0;
-        
-        if(dp[n] != -1)
-            return dp[n];
+    int solveTab(int n){
+        vector<int> dp(n+1, INT_MAX);
+        dp[0] = 0;
         
         int ans = n;
-        for(int i=1; i*i <= n; i++){
-            int temp = i*i;
-            ans = min(ans, 1 + solveMem(n-temp, dp));
+        for(int i=1; i<=n; i++){
+            for(int j=1; j*j <= n; j++){
+                int temp = j*j;
+                if(i-temp >= 0)
+                    dp[i] = min(dp[i], 1 + dp[i - temp]);
+            }
         }
 
-        dp[n] = ans;
-        
         return dp[n];
     }
 public:
     int numSquares(int n) {
-        vector<int> dp(n+1, -1);
-        return solveMem(n, dp);
+        return solveTab(n);
     }
 };
