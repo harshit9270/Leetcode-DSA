2800b086642f7c42688a5138bd406ba042a34098
Harshit Sharma <47037260+harshit9270@users.noreply.github.com>
2023-03-14 16:06:31 +0530

    Time: 0 ms (100.00%), Space: 6 MB (46.18%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,14 +1,16 @@
-// Bottom up DP
+// Space optimized DP
 class Solution {
     int solve(int n){
-        vector<int> dp(n+2);
-        dp[1] = 1;
-        dp[2] = 2;
+        int prev2=1,prev1=1; // ** initally at 0th and 1st index
         
-        for(int i=3; i<=n; i++)
-            dp[i] = dp[i-1] + dp[i-2];
+        for(int i=2; i<=n; i++){
+            int curr = prev1 + prev2;
+            
+            prev2 = prev1;
+            prev1 = curr;
+        }
         
-        return dp[n];
+        return prev1;
     }
 public:
     int climbStairs(int n) {        
