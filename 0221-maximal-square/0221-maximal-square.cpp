88d06207d82cc8aa5e2e91b53b1317cd43f6f80f
Harshit Sharma <47037260+harshit9270@users.noreply.github.com>
2023-03-16 20:41:54 +0530

    Time: 87 ms (79.56%), Space: 21 MB (23.20%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -1,32 +1,31 @@
-// Top down DP
+// Bottom up DP
 class Solution {
-    int solveMem(vector<vector<char>>& matrix, int i, int j, int& maxi, vector<vector<int>>& dp){
-        if(i >= matrix.size() || j >= matrix[0].size())
-            return 0;
+    int solveTab(vector<vector<char>>& matrix, int& maxi){
+        int n = matrix.size(), m = matrix[0].size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
         
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        
-        int right = solveMem(matrix, i, j+1, maxi, dp);
-        int diagonal = solveMem(matrix, i+1, j+1, maxi, dp);
-        int down = solveMem(matrix, i+1, j, maxi, dp);
-        
-        if(matrix[i][j] == '1'){
-            dp[i][j] = 1 + min(right, min(diagonal, down));
-            maxi = max(maxi, dp[i][j]);
-            return dp[i][j];
+        for(int i=n-1; i>=0; i--){
+            for(int j=m-1; j>=0; j--){
+                int right = dp[i][j+1];
+                int diagonal = dp[i+1][j+1];
+                int down = dp[i+1][j];
+                
+                if(matrix[i][j] == '1'){
+                    dp[i][j] = 1 + min(right, min(diagonal, down));
+                    maxi = max(maxi, dp[i][j]);
+                }
+                else
+                    dp[i][j] = 0;   
+            }
         }
-        else
-            return dp[i][j] = 0;        
+        
+        return dp[0][0];
     }
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
-        int n = matrix.size(), m = matrix[0].size();
         int maxi = 0;
         
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        
-        solveMem(matrix, 0, 0, maxi, dp);
+        solveTab(matrix, maxi);
         return maxi*maxi;
     }
 };
