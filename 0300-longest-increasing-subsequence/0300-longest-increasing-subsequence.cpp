bbdc5592d15a01e14569d6769be433820755b8c5
Harshit Sharma <47037260+harshit9270@users.noreply.github.com>
2023-03-24 13:26:05 +0530

    Time: 729 ms (23.76%), Space: 292.2 MB (17.58%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,27 +1,28 @@
-// Top Down DP
+// Bottom Up DP
 class Solution {
-    int solveMem(vector<int>& nums, int curr, int prev, vector<vector<int>> & dp){
-        if(curr == nums.size())
-            return 0;
+    int solveTab(vector<int>& nums){
+        int n = nums.size();
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
         
-        if(dp[curr][prev+1] != -1)
-            return dp[curr][prev+1];
+        for(int curr = n-1; curr >= 0; curr--){
+            for(int prev = curr-1; prev >= -1; prev--){
+                // include
+                int take = 0;
+                if(prev == -1 || nums[curr] > nums[prev])
+                    take = 1 + dp[curr+1][curr+1];
+
+                // exclude
+                int notTake = 0 + dp[curr+1][prev+1];
+                
+                dp[curr][prev + 1] = max(take, notTake);
+            }
+        }
         
-        // include
-        int take = 0;
-        if(prev == -1 || nums[curr] > nums[prev])
-            take = 1 + solveMem(nums, curr+1, curr, dp);
         
-        // exclude
-        int notTake = 0 + solveMem(nums, curr+1, prev, dp);
-        
-        return dp[curr][prev+1] = max(take, notTake);        
+        return dp[0][-1+1];        
     }
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n+1, -1));
-        
-        return solveMem(nums, 0, -1, dp);
+        return solveTab(nums);
     }
 };
