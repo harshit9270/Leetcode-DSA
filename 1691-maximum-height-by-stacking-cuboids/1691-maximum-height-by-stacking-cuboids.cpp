d34e0abeebe78daa9d750ba951f2a698a22dae8d
Harshit Sharma <47037260+harshit9270@users.noreply.github.com>
2023-03-24 21:34:59 +0530

    Time: 85 ms (18.91%), Space: 21.5 MB (25.27%) - LeetHub

---
+++ 1691-maximum-height-by-stacking-cuboids/1691-maximum-height-by-stacking-cuboids.cpp
@@ -0,0 +1,43 @@
+class Solution {
+    bool check(vector<int> base, vector<int> newBox){
+        if( (newBox[0] <= base[0]) && (newBox[1] <= base[1]) && (newBox[2] <= base[2]))
+            return true;
+        return false;
+    }
+
+    int solveSpaceOptimalLIS(int n, vector<vector<int>>& a){
+        vector<int> currRow(n+1, 0);
+        vector<int> nextRow(n+1, 0);
+        
+        for(int curr = n-1; curr>=0; curr--){
+            for(int prev = curr-1; prev>=-1; prev--){
+                // include
+                int take = 0;
+                if(prev == -1 || check(a[curr], a[prev]))
+                    take = a[curr][2] + nextRow[curr+1];
+                    
+                // exclude
+                int notTake = 0 + nextRow[prev+1];
+                
+                currRow[prev+1] = max(take, notTake);
+            }
+            
+            nextRow = currRow;
+        }
+        
+        return nextRow[0];
+    }
+public:
+    int maxHeight(vector<vector<int>>& cuboids) {
+        // STEP - 1: Sort all dimensions for every cuboid
+        for(auto &a: cuboids)
+            sort(a.begin(), a.end()); 
+
+        // STEP - 2: Solve all cuboid basis on w or 1
+        sort(cuboids.begin(), cuboids.end());
+
+        // STEP - 3: Use LIS Logic
+        int n = cuboids.size();
+        return solveSpaceOptimalLIS(n, cuboids);
+    }
+};
