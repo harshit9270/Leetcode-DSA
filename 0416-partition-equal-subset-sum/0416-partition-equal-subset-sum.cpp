688c8e4c267307aca4d918425ffbff9a5fa666d6
Harshit Sharma <47037260+harshit9270@users.noreply.github.com>
2023-03-25 21:52:50 +0530

    Time: 373 ms (42.08%), Space: 11.4 MB (83.14%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,26 +1,27 @@
-// Bottom up DP
+// Space Optimized DP Approach
 class Solution{
 private: 
     bool solveTab(vector<int>&arr, int total){
         int n = arr.size();
-        vector<vector<int>> dp(n+1, vector<int>(total/2 +1, 0));
+        vector<int> curr(total/2 +1, 0);
+        vector<int> next(total/2 +1, 0);
         
-        for(int i = 0; i <= n; i++)
-            dp[i][0] = 1;
+        curr[0] = 1; next[0] = 1;
         
         for(int index = n-1; index >= 0; index--){
             for(int target = 0; target <= total/2; target++){
                 bool incl = 0;
                 if(target - arr[index] >= 0)
-                    incl = dp[index+1][target - arr[index]];
+                    incl = next[target - arr[index]];
                 
-                bool excl = dp[index+1][target];
+                bool excl = next[target];
                 
-                dp[index][target] = incl or excl;
+                curr[target] = incl or excl;
             }
+            next = curr;
         }
         
-        return dp[0][total/2];
+        return next[total/2];
     }
     
 public:
