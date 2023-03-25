ad408c41b95c17d42805685328f99bffa4e8a65e
Harshit Sharma <47037260+harshit9270@users.noreply.github.com>
2023-03-25 21:50:33 +0530

    Time: 427 ms (31.84%), Space: 95.8 MB (20.84%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,23 +1,26 @@
 // Bottom up DP
 class Solution{
 private: 
-    bool solveTab(vector<int>&arr, int target){
+    bool solveTab(vector<int>&arr, int total){
         int n = arr.size();
-        vector<vector<int>> dp(n+1, vector<int>(target+1, 0));
+        vector<vector<int>> dp(n+1, vector<int>(total/2 +1, 0));
         
         for(int i = 0; i <= n; i++)
             dp[i][0] = 1;
         
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= target; j++){
-                if(arr[i-1] <= j)
-                    dp[i][j] = dp[i-1][j-arr[i-1]] || dp[i-1][j];
-                else
-                    dp[i][j] = dp[i-1][j];
+        for(int index = n-1; index >= 0; index--){
+            for(int target = 0; target <= total/2; target++){
+                bool incl = 0;
+                if(target - arr[index] >= 0)
+                    incl = dp[index+1][target - arr[index]];
+                
+                bool excl = dp[index+1][target];
+                
+                dp[index][target] = incl or excl;
             }
         }
         
-        return dp[n][target];
+        return dp[0][total/2];
     }
     
 public:
@@ -31,8 +34,6 @@ public:
         if(total % 2 != 0) 
             return false;
         
-        int target = total/2;
-        
-        return solveTab(arr, target);
+        return solveTab(arr, total);
     }
 };
