6f953fa600eb5f7064da02e9f17c1be15d0765df
Harshit Sharma <47037260+harshit9270@users.noreply.github.com>
2023-03-25 12:00:18 +0530

    Time: 15 ms (92.44%), Space: 8.5 MB (88.08%) - LeetHub

--- 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.cpp
+++ 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.cpp
@@ -1,34 +1,39 @@
-// Bottom Up DP
+// Space Optimized DP Approach
 class Solution {
-    int solveTab(vector<int>& slices){
+    int solveSpaceOP(vector<int>& slices){
         int k = slices.size();
-        vector<vector<int>> dp1(k+2, vector<int>(k+2, 0));
-        vector<vector<int>> dp2(k+2, vector<int>(k+2, 0));
+        
+        vector<int> prev1(k+2, 0), curr1(k+2, 0), next1(k+2, 0);
+        vector<int> prev2(k+2, 0), curr2(k+2, 0), next2(k+2, 0);
         
         for(int index = k-2; index >= 0; index--){
             for(int n = 1; n <= k/3; n++){
-                int take = slices[index] + dp1[index+2][n-1];
-                int notTake = 0 + dp1[index+1][n];
+                int take = slices[index] + next1[n-1];
+                int notTake = 0 + curr1[n];
 
-                dp1[index][n] = max(take, notTake);
+                prev1[n] = max(take, notTake);
             }
+            next1 = curr1;
+            curr1 = prev1;
         }
-        int case1 = dp1[0][k/3];
+        int case1 = curr1[k/3];
         
         for(int index = k-1; index >= 1; index--){
             for(int n = 1; n <= k/3; n++){
-                int take = slices[index] + dp2[index+2][n-1];
-                int notTake = 0 + dp2[index+1][n];
+                int take = slices[index] + next2[n-1];
+                int notTake = 0 + curr2[n];
 
-                dp2[index][n] = max(take, notTake);
+                prev2[n] = max(take, notTake);
             }
+            next2 = curr2;
+            curr2 = prev2;
         }
-        int case2 = dp2[1][k/3];
+        int case2 = curr2[k/3];
         
         return max(case1, case2);        
     }
 public:
     int maxSizeSlices(vector<int>& slices) {
-        return solveTab(slices);
+        return solveSpaceOP(slices);
     }
 };
