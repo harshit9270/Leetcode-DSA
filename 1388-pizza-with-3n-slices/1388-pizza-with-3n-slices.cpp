242634c5603350fdf9814a084fbae72833d9d790
Harshit Sharma <47037260+harshit9270@users.noreply.github.com>
2023-03-25 11:56:28 +0530

    Time: 20 ms (77.33%), Space: 23.3 MB (8.50%) - LeetHub

--- 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.cpp
+++ 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.cpp
@@ -1,27 +1,34 @@
-// Top Down DP
+// Bottom Up DP
 class Solution {
-    int solveMem(int index, int endIndex, vector<int>& slices, int n, vector<vector<int>>& dp){
-        if(n == 0 || index > endIndex)
-            return 0;
+    int solveTab(vector<int>& slices){
+        int k = slices.size();
+        vector<vector<int>> dp1(k+2, vector<int>(k+2, 0));
+        vector<vector<int>> dp2(k+2, vector<int>(k+2, 0));
         
-        if(dp[index][n] != -1)
-            return dp[index][n];
+        for(int index = k-2; index >= 0; index--){
+            for(int n = 1; n <= k/3; n++){
+                int take = slices[index] + dp1[index+2][n-1];
+                int notTake = 0 + dp1[index+1][n];
+
+                dp1[index][n] = max(take, notTake);
+            }
+        }
+        int case1 = dp1[0][k/3];
         
-        int take = slices[index] + solveMem(index+2, endIndex, slices, n-1, dp);
-        int notTake = 0 + solveMem(index+1, endIndex, slices, n, dp);
+        for(int index = k-1; index >= 1; index--){
+            for(int n = 1; n <= k/3; n++){
+                int take = slices[index] + dp2[index+2][n-1];
+                int notTake = 0 + dp2[index+1][n];
+
+                dp2[index][n] = max(take, notTake);
+            }
+        }
+        int case2 = dp2[1][k/3];
         
-        return dp[index][n] = max(take, notTake);        
+        return max(case1, case2);        
     }
 public:
     int maxSizeSlices(vector<int>& slices) {
-        int k = slices.size();
-        
-        vector<vector<int>> dp1(k, vector<int>(k, -1));
-        int case1 = solveMem(0, k-2, slices, k/3, dp1);
-        
-        vector<vector<int>> dp2(k, vector<int>(k, -1));
-        int case2 = solveMem(1, k-1, slices, k/3, dp2);
-        
-        return max(case1, case2);
+        return solveTab(slices);
     }
 };
