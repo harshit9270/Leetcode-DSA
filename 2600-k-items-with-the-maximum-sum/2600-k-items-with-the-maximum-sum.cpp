653d77b48439457006e31b10bad7853ad4bc99ff
Harshit Sharma <47037260+harshit9270@users.noreply.github.com>
2023-03-26 19:39:35 +0530

    Time: 2 ms (72.22%), Space: 6 MB (11.11%) - LeetHub

--- 2600-k-items-with-the-maximum-sum/2600-k-items-with-the-maximum-sum.cpp
+++ 2600-k-items-with-the-maximum-sum/2600-k-items-with-the-maximum-sum.cpp
@@ -1,37 +1,8 @@
 class Solution {
 public:
     int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
-        int sum = 0;
-        
-        if(k > 0){
-            if(numOnes < k){
-                k -= numOnes;
-                sum += numOnes;
-            }
-            else{
-                sum += k;
-                k = 0;
-            }
-        }
-        if(k > 0){
-            if(numZeros < k){
-                k -= numZeros;
-            }
-            else{
-                k = 0;
-            }
-        }
-        if(k > 0){
-            if(numNegOnes < k){
-                k -= numNegOnes;
-                sum -= k;
-            }
-            else{
-                sum -= k;
-                k = 0;
-            }
-        }
-        
-        return sum;
+        if(k <= numOnes) return k;
+        if(k <= numOnes + numZeros) return numOnes;
+        return numOnes - (k - numOnes - numZeros);
     }
 };
