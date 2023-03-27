90b9847a6384d001dc82d0ffce2da6a637e271c5
Harshit Sharma <47037260+harshit9270@users.noreply.github.com>
2023-03-27 21:58:49 +0530

    Time: 180 ms (95.63%), Space: 90.2 MB (92.49%) - LeetHub

--- 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.cpp
+++ 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.cpp
@@ -1,36 +1,46 @@
-// Bottom Up DP
+// Space Optimized DP Approach
 class Solution {
-    int solveTab(vector<int>& nums1, vector<int>& nums2){
+    int solveSpaceOP(vector<int>& nums1, vector<int>& nums2){
         int n = nums1.size();
-        vector<vector<int>> dp(n+1, vector<int>(2, 0));
+        
+        int swap = 0, noSwap = 0;
+        int currSwap = 0, currNoSwap = 0;
         
         for(int index = n-1; index >= 1; index--){
             for(int swapped = 1; swapped >= 0; swapped--){
                 int ans = 1e9;
                 int prev1 = nums1[index-1], prev2 = nums2[index-1];
 
-                if(swapped)
-                    swap(prev1, prev2);
+                if(swapped){
+                    int temp = prev1;
+                    prev1 = prev2;
+                    prev2 = temp;
+                }
 
                 // no swap
                 if(nums1[index] > prev1 && nums2[index] > prev2)
-                    ans = dp[index+1][0];
+                    ans = noSwap;
 
                 // swap
                 if(nums1[index] > prev2 && nums2[index] > prev1)
-                    ans = min(ans, 1 + dp[index+1][1]);
+                    ans = min(ans, 1 + swap);
 
-                dp[index][swapped] = ans;
+                if(swapped)
+                    currSwap = ans;
+                else
+                    currNoSwap = ans;
             }
+            swap = currSwap;
+            noSwap = currNoSwap;
         }
         
-        return dp[1][0];
+        return min(swap, noSwap);
     }
 public:
     int minSwap(vector<int>& nums1, vector<int>& nums2) {
         nums1.insert(nums1.begin(), -1);
         nums2.insert(nums2.begin(), -1);
         
-        return solveTab(nums1, nums2);
+        return solveSpaceOP(nums1, nums2);
     }
 };
