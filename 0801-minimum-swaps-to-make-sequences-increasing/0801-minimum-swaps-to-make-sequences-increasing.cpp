9fea1b75eb84d9569b11a172702082a1d44bde13
Harshit Sharma <47037260+harshit9270@users.noreply.github.com>
2023-03-27 21:54:37 +0530

    Time: 268 ms (65.25%), Space: 120.4 MB (60.54%) - LeetHub

--- 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.cpp
+++ 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.cpp
@@ -1,35 +1,36 @@
-// Top Down DP
+// Bottom Up DP
 class Solution {
-    int solveMem(vector<int>& nums1, vector<int>& nums2, int index, bool swapped, vector<vector<int>>& dp){
-        if(index == nums1.size())
-            return 0;
+    int solveTab(vector<int>& nums1, vector<int>& nums2){
+        int n = nums1.size();
+        vector<vector<int>> dp(n+1, vector<int>(2, 0));
         
-        if(dp[index][swapped] != -1)
-            return dp[index][swapped];
+        for(int index = n-1; index >= 1; index--){
+            for(int swapped = 1; swapped >= 0; swapped--){
+                int ans = 1e9;
+                int prev1 = nums1[index-1], prev2 = nums2[index-1];
+
+                if(swapped)
+                    swap(prev1, prev2);
+
+                // no swap
+                if(nums1[index] > prev1 && nums2[index] > prev2)
+                    ans = dp[index+1][0];
+
+                // swap
+                if(nums1[index] > prev2 && nums2[index] > prev1)
+                    ans = min(ans, 1 + dp[index+1][1]);
+
+                dp[index][swapped] = ans;
+            }
+        }
         
-        int ans = 1e9;
-        int prev1 = nums1[index-1], prev2 = nums2[index-1];
-        
-        if(swapped)
-            swap(prev1, prev2);
-        
-        // no swap
-        if(nums1[index] > prev1 && nums2[index] > prev2)
-            ans = solveMem(nums1, nums2, index+1, 0, dp);
-        
-        // swap
-        if(nums1[index] > prev2 && nums2[index] > prev1)
-            ans = min(ans, 1 + solveMem(nums1, nums2, index+1, 1, dp));
-        
-        return dp[index][swapped] = ans;
+        return min(dp[1][0], dp[1][1]);
     }
 public:
     int minSwap(vector<int>& nums1, vector<int>& nums2) {
         nums1.insert(nums1.begin(), -1);
         nums2.insert(nums2.begin(), -1);
         
-        vector<vector<int>> dp(nums1.size(), vector<int>(2, -1));
-        
-        return solveMem(nums1, nums2, 1, 0, dp);
+        return solveTab(nums1, nums2);
     }
 };
