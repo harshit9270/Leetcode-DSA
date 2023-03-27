1edf45d131638b348d124cbc1c5829d4aa5096d5
Harshit Sharma <47037260+harshit9270@users.noreply.github.com>
2023-03-27 20:29:34 +0530

    Time: 6 ms (93.15%), Space: 10.1 MB (61.86%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,26 +1,29 @@
-// Top Down DP
+// Bottom Up DP
 class Solution {
-    int solveMem(vector<vector<int>>& grid, int i, int j, vector<vector<int>>& dp){
+    int solveTab(vector<vector<int>>& grid){
         int m = grid.size(), n = grid[0].size();
-        if(i == m-1 && j == n-1)
-            return grid[i][j];
+        vector<vector<int>> dp(m, vector<int>(n, 0));
         
-        if(i == m || j == n)
-            return 1e9;
+        for(int i=m-1; i>=0; i--){
+            for(int j=n-1; j>=0; j--){
+                if(i == m-1 and j == n-1)
+                    dp[i][j]=grid[i][j];
+                else{
+                    int down = 1e9, right = 1e9;
+                    if(i < m-1) // cannot go right if(i == m-1)
+                        down = dp[i+1][j];
+                    if(j < n-1) // cannot do down if(j == n-1)
+                        right = dp[i][j+1];
+                    
+                    dp[i][j] = grid[i][j] + min(down, right);
+                }
+            }
+        }
         
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        
-        int down = grid[i][j] + solveMem(grid, i+1, j, dp);
-        int right = grid[i][j] + solveMem(grid, i, j+1, dp);
-        
-        return dp[i][j] = min(down, right);
+        return dp[0][0];
     }
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
-        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
-        
-        return solveMem(grid, 0, 0, dp);
+        return solveTab(grid);
     }
 };
