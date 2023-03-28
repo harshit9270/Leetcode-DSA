43d0935d4ad75dbef136c17184fd77c898bbe303
Harshit Sharma <47037260+harshit9270@users.noreply.github.com>
2023-03-28 09:40:16 +0530

    Time: 1915 ms (50.21%), Space: 242.6 MB (54.91%) - LeetHub

---
+++ 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.cpp
@@ -0,0 +1,34 @@
+// Bottom Up DP
+class Solution {
+     int solveTab(vector<int>& A){
+        int n = A.size();
+        if(n <= 2)
+            return n;
+            
+        int ans = 0;
+        unordered_map<int, int> dp[n+1];
+        
+        for(int i = 1; i<n; i++){
+            for(int j = 0; j<i; j++){
+                int diff = A[i] - A[j];
+                
+                int cnt = 1;
+                
+                // check if answer already present
+                if(dp[j].count(diff)){
+                    cnt = dp[j][diff];
+                }
+                
+                dp[i][diff] = 1 + cnt;
+                
+                ans = max(ans, dp[i][diff]);
+            }
+        }
+        
+        return ans;
+    }
+public:
+    int longestArithSeqLength(vector<int>& nums) {        
+        return solveTab(nums);
+    }
+};
