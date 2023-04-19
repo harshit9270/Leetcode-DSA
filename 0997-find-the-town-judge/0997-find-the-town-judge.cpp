1f00198658af1bb83142791495dc1e16069b0387
Harshit Sharma <47037260+harshit9270@users.noreply.github.com>
2023-04-19 13:03:17 +0530

    Time: 225 ms (23.13%), Space: 68.9 MB (27.69%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,17 +1,23 @@
+// Using indegree and outdegree
 class Solution {
 public:
-    int findJudge(int N, vector<vector<int>>& trust) {
-        vector<int> Trusted(N + 1, 0);
-        for(auto person : trust){
-            Trusted[person[0]]--;
-            Trusted[person[1]]++;
+    int findJudge(int n, vector<vector<int>>& trust) {
+        if(trust.empty() && n==1)
+            return 1;
+
+        vector<int> out(n+1,0);
+        vector<int> in(n+1,0);
+
+        for(auto edge : trust){
+            out[edge[0]]++;
+            in[edge[1]]++;
         }
-        
-        for(int i = 1;i <= N;i++){
-            if(Trusted[i] == N - 1)
+
+        for(int i=0;i<=n;i++){
+            if(out[i]==0 && in[i]==n-1)
                 return i;
         }
-        
+
         return -1;
     }
 };
