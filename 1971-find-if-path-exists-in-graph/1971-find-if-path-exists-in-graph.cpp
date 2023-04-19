3b9c1e5611033c2d325ad15397d2c67ef3091b0d
Harshit Sharma <47037260+harshit9270@users.noreply.github.com>
2023-04-19 12:57:09 +0530

    Time: 850 ms (29.04%), Space: 200.6 MB (44.00%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,24 +1,32 @@
+// Here we are exploiting the property of bfs of stopping traversal once a component is completely traversed, 
+// Once a component is traversed it will not traverse another component by itself
+// Since, valid path does not exists for disconnected start and end
+
 class Solution {
 public:
     bool validPath(int n, vector<vector<int>>& edges, int start, int end) {
-        unordered_map<int,vector<int>> graph; 
+        // adj list
+        unordered_map<int,vector<int>> adj; 
         for(auto e : edges) {
-            graph[e[0]].push_back(e[1]);
-            graph[e[1]].push_back(e[0]);
+            adj[e[0]].push_back(e[1]);
+            adj[e[1]].push_back(e[0]);
         }
+        
         vector<bool> visited(n,0);        
         queue<int> q;
         q.push(start);
         visited[start] = 1; 
+        
         while(!q.empty()) {
             int curr = q.front();
             q.pop();
+            
             if(curr == end)
                 return 1; 
-            for(auto &node : graph[curr]){
-                if(!visited[node]){
-                    visited[node] = 1; 
-                    q.push(node);
+            for(auto & nbr : adj[curr]){
+                if(!visited[nbr]){
+                    visited[nbr] = 1; 
+                    q.push(nbr);
                 }
             }
         }
