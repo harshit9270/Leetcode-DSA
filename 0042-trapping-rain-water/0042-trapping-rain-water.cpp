8b63bbff8e8b51aa4da148fdd803cd62ebe16aca
Harshit Sharma <47037260+harshit9270@users.noreply.github.com>
2023-05-02 21:16:36 +0530

    Time: 8 ms (96.47%), Space: 19.7 MB (85.43%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -0,0 +1,29 @@
+// Two Pointers Approach
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int left = 0, right = n - 1;
+        int left_max = 0, right_max = 0;
+        int water_stored = 0;
+
+        while (left < right) {
+            if (height[left] < height[right]) {
+                if (height[left] >= left_max) {
+                    left_max = height[left];
+                } else {
+                    water_stored += left_max - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= right_max) {
+                    right_max = height[right];
+                } else {
+                    water_stored += right_max - height[right];
+                }
+                right--;
+            }
+        }
+        return water_stored;
+    }
+};
