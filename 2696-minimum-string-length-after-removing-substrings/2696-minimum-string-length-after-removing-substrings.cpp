db29e68f2e315bfe6cc8dd3bbbffa3cd014ee86e
Harshit Sharma <47037260+harshit9270@users.noreply.github.com>
2023-06-01 20:59:46 +0530

    Time: 9 ms (58.68%), Space: 8.1 MB (18.57%) - LeetHub

--- 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
@@ -3,21 +3,16 @@ class Solution {
 public:
     int minLength(string s) {
         stack<char> st;
-        int minLen = s.size();
         
         for(char ch : s){
-            if(ch == 'B' && !st.empty() && st.top() == 'A'){
+            if(ch == 'B' && !st.empty() && st.top() == 'A')
                 st.pop();
-                minLen -= 2;
-            }
-            else if(ch == 'D' && !st.empty() && st.top() == 'C'){
+            else if(ch == 'D' && !st.empty() && st.top() == 'C')
                 st.pop();
-                minLen -= 2;
-            }
             else
                 st.push(ch);
         }
         
-        return minLen;
+        return st.size();
     }
 };
