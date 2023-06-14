f2f86cde18c792ffd5b987b8d69661077f8697f7
Harshit Sharma <47037260+harshit9270@users.noreply.github.com>
2023-06-14 23:14:45 +0530

    Time: 404 ms (6.64%), Space: 40.1 MB (92.61%) - LeetHub

---
+++ 0705-design-hashset/0705-design-hashset.cpp
@@ -0,0 +1,52 @@
+class MyHashSet {
+public:
+    struct ListNode {
+        int val;
+        ListNode *next;
+        ListNode() : val(0), next(NULL) {}
+        ListNode(int x) : val(x), next(NULL) {}
+        ListNode(int x, ListNode *next) : val(x), next(next) {}
+    };
+    
+    ListNode* head;
+    
+    MyHashSet() {
+        head = new ListNode(-1);
+    }
+    
+    void add(int key) {
+        ListNode* temp = new ListNode(key);
+        ListNode* curr = head;
+        while(curr and curr->next) {
+            if(curr->next->val == key)    return;
+            curr = curr->next;
+        }
+        curr->next = temp;
+    }
+    
+    void remove(int key) {
+        ListNode* temp = head;
+        while(temp and temp->next) {
+            if(temp->next->val == key)
+                temp->next = temp->next->next;
+            temp = temp->next;
+        }
+    }
+    
+    bool contains(int key) {
+        ListNode* temp = head;
+        while(temp) {
+            if(temp->val == key)    return true;
+            temp = temp->next;
+        }
+        return false;
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
