1e48428829cf9aaae42fbc150b154d14d5849347
Harshit Sharma <47037260+harshit9270@users.noreply.github.com>
2023-06-24 22:25:20 +0530

    Time: 27 ms (80.52%), Space: 12.1 MB (71.49%) - LeetHub

---
+++ 2712-minimum-cost-to-make-all-characters-equal/2712-minimum-cost-to-make-all-characters-equal.cpp
@@ -0,0 +1,14 @@
+// Greedy Approach \U0001f60d
+class Solution {
+public:
+    long long minimumCost(string s) {
+        long long ans = 0;
+        int n = s.size();
+        
+        for(int i=1;i<n;i++)
+            if(s[i] != s[i-1])
+                ans += min(i,n-i); // Key Observation
+            
+        return ans;
+    }
+};
