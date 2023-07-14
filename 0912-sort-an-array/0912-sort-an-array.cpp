991a77ec9fb2ae6f70319225d4ce7680354c6db4
Harshit Sharma <47037260+harshit9270@users.noreply.github.com>
2023-07-14 23:37:02 +0530

    Time: 594 ms (36.26%), Space: 144.6 MB (41.27%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,19 +1,40 @@
-// Using priority queue
 class Solution {
 public:
-    vector<int> sortArray(vector<int>& nums) {
-        // min heap
-        priority_queue<int, vector<int>, greater<int>> pq;
-        
-        for(int i : nums)
-            pq.push(i);
-        
-        int i = 0;
-        while(! pq.empty()){
-            nums[i ++] = pq.top();
-            pq.pop();
+    void merge(vector<int>& arr, int left, int mid, int right) {
+        vector<int> temp(right - left + 1);
+        int i = left, j = mid + 1, k = 0;
+
+        while (i <= mid && j <= right) {
+            if (arr[i] < arr[j])
+                temp[k++] = arr[i++];
+            else
+                temp[k++] = arr[j++];
         }
-        
+
+        while (i <= mid)
+            temp[k++] = arr[i++];
+
+        while (j <= right)
+            temp[k++] = arr[j++];
+
+        for (k = 0; k < temp.size(); ++k)
+            arr[k + left] = temp[k];
+    }
+    
+    void mergeSort(vector<int>& arr, int left, int right) {
+        if (left >= right)
+            return;
+
+        int mid = left + (right - left) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+
+    vector<int> sortArray(vector<int>& nums) {
+        int left = 0;
+        int right = nums.size() - 1;
+        mergeSort(nums, left, right);
         return nums;
     }
 };
